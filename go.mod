65f94d1fe3dadb1cf99ec0b3556d97b8be83d41d
陈颢琦 <13436010+shizuizaodezaoan@user.noreply.gitee.com>
2024-02-26 16:26:44 +0800

    five commit

--- go.mod
+++ go.mod
@@ -2,10 +2,17 @@ module github.com/zuizaodezaoan/formwork
 
 go 1.21.1
 
-require google.golang.org/grpc v1.62.0
+require (
+	google.golang.org/grpc v1.62.0
+	gorm.io/driver/mysql v1.5.4
+	gorm.io/gorm v1.25.7
+)
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ model/join.go
@@ -0,0 +1,42 @@
+package model
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+var err error
+
+func init() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/area?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger_web
+			ParameterizedQueries:      true,        // Don't include params in the SQL logs
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+}
+
+func Transaction(txc func(tx *gorm.DB) error) {
+	tx := DB.Begin()
+	err = txc(tx)
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+	tx.Commit()
+}

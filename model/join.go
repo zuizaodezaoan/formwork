6a0192b92d76528f276edb0e8c838453d649520b
陈颢琦 <13436010+shizuizaodezaoan@user.noreply.gitee.com>
2024-02-26 20:00:59 +0800

    第四次

--- model/join.go
+++ model/join.go
@@ -14,7 +14,7 @@ var DB *gorm.DB
 var err error
 
 func InitMysql() {
-	dsn := "root:root@tcp(127.0.0.1:3306)/area?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/shop?charset=utf8mb4&parseTime=True&loc=Local"
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{

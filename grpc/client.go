dde9c4c99b39d9307b2e6d4b5cffee640efd04fb
陈颢琦 <13436010+shizuizaodezaoan@user.noreply.gitee.com>
2024-03-14 21:29:26 +0800

    主分支；

--- grpc/client.go
+++ grpc/client.go
@@ -23,7 +23,7 @@ func RegisterApi(serverName string) (*grpc.ClientConn, error) {
 		return nil, err
 	}
 	log.Println("54343234543", host, port)
-	conn, err := grpc.Dial(fmt.Sprintf("consul://10.2.171.28:8081/"+serverName+"?wait=14s", grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`)))
+	conn, err := grpc.Dial(fmt.Sprintf("consul://10.2.171.28:8500/"+"shop"+"?wait=14s", grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`)))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 		return nil, err

664d8a860e37e69d1a089eb29d413c7a3b6c3359
陈颢琦 <13436010+shizuizaodezaoan@user.noreply.gitee.com>
2024-03-14 20:31:04 +0800

    主分支；

--- api/api.go
+++ api/api.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"context"
+
+	"github.com/zuizaodezaoan/formwork/consul"
 	"github.com/zuizaodezaoan/formwork/model"
 	"github.com/zuizaodezaoan/formwork/nacos"
 )
@@ -9,6 +12,11 @@ func Init(serverName string, str ...string) error {
 	var err error
 	//err = nacos.InitConfig()
 	err = nacos.InitNacos()
+
+	_, err = consul.InitRegisterServer(context.Background(), "user_srv.g5")
+	if err != nil {
+		return err
+	}
 
 	nacos.Enroll()
 
